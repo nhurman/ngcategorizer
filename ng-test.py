67c60de4e16ce3fdf2f7c34a3f05becafe8ed681
Alexandre Leonardi <alexandre.leonardi@free.fr>
2015-04-01 00:07:55 +0200

    Implementation of a stemmer

--- ng-test.py
+++ ng-test.py
@@ -2,9 +2,11 @@
 
 import os
 from ngcategorizer.NGCategorizer import NGCategorizer
+from ngcategorizer.NGCategorizerStemmer import NGCategorizerStemmer
 from ngcategorizer.PerformanceEvaluator import PerformanceEvaluator
 
-ng = NGCategorizer('Sujet/20_newsgroups')
+""" Choose between NGCategorizer and NGCategorizerStemmer here to use stemmer or not """
+ng = NGCategorizerStemmer('Sujet/20_newsgroups')
 save_path = "Tests/wfreqs-training.p"
 groups = ng.exploreDirectory()
 training, testing = ng.splitSet(groups)
